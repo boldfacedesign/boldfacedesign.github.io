b7a8dbbca6e1168e93571358d3c909b6381862eb
davidparker <davidparkerdesign@gmail.com>
2014-07-17 16:42:41 +0100

    added more files and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,40 @@
 var gulp = require('gulp'),
 	util = require('gulp-util'),
-	kss = require('gulp-kss');
+	kss = require('gulp-kss'),
+	sourcemaps = require('gulp-sourcemaps'),
+	uglify = require('gulp-uglify');
+
+var paths = {
+  js: ['js/*.js', '!js/libs/*'],
+  images: 'images/site/*',
+  html: 'html/partials/*'
+};
 
 
 
+// Minify and copy all JavaScript (except vendor scripts)
+gulp.task('scripts-min', ['clean'], function() {
+	return gulp.src(paths.scripts)
+    	.pipe(uglify())
+    	.pipe(rename(function (path) {
+    		path.basename += ".min";
+    	})
+    	.pipe(gulp.dest('js/min'));
+});
+
+
+gulp.task('images', function() {
+	return gulp.src(paths.images)
+		.pipe(imagemin({
+            progressive: true,
+            optimizationLevel: 3,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('images/site-min'));
+});
+
+//Compile auto generated styleguides using node KSS
 gulp.task('kss-css', function() {
 	return gulp.src(['css/master.css'])             //get compiled CSS
 		.pipe(rename('style.css'))                    //rename to KSS required nameing convention
@@ -21,4 +52,12 @@ gulp.task('kss', ['kss-css', 'kss-sprite'], function(){
 			overview: __dirname + '/guides/styleguide.md'   //define markdown file
 		}))
 		.pipe(gulp.dest('guides/complete'));              //define compile folder
+});
+
+
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.js, ['scripts']);
+  gulp.watch(paths.images, ['images']);
 });
